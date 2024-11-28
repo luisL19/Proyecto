148036b7c0c925812cb8a68dc754c82fd96dafae
luisL19 <lugobecerra9@gmail.com>
2024-11-27 23:41:59 -0500

    cambios para la nueva entrega

--- helpers/menu.js
+++ helpers/menu.js
@@ -1,66 +1,94 @@
-const colors = require('colors');
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
+const color = require("colors");
+const TareaManager = require("../models/tarea");
+
 
 const preguntas = [
   {
-    type: 'list',
-    name: 'options',
-    message: '¿Qué quieres hacer?',
+    type: "list",
+    name: "options",
+    message: "Qué quieres hacer?",
     choices: [
-      { value: '1', name: `${'1.'.red} Crear tarea` },
-      { value: '2', name: `${'2.'.red} Listar tareas` },
-      { value: '3', name: `${'3.'.red} Listar tareas completas` },
-      { value: '4', name: `${'4.'.red} Listar tareas pendientes` },
-      { value: '5', name: `${'5.'.red} Completar tarea(s)` },
-      { value: '6', name: `${'6.'.red} Borrar tarea` },
-      { value: '0', name: `${'0.'.red} Salir` },
+      { value: "1", name: "1. Crear tarea" },
+      { value: "2", name: "2. Listar tareas" },
+      { value: "3", name: "3. Listar tareas completas" },
+      { value: "4", name: "4. Listar tareas pendientes" },
+      { value: "5", name: "5. Completar tarea(s)" },
+      { value: "6", name: "6. Borrar tarea" },
+      { value: "0", name: "0. Salir" },
     ],
   },
 ];
 
-const menu = async () => {
-  console.clear();
-  console.log('····························································'.green);
-  console.log('           First Application'.white);
-  console.log('····························································'.green);
 
-  const { options } = await inquirer.prompt(preguntas);
-  return options;
+
+const menu = async () => {
+  
+  console.log(`${"°°°°°°°°°°°°°°°°°°°°°°°°".green}\n${"Gestor de tareas".blue}\n${"°°°°°°°°°°°°°°°°°°°°°°°°".green}`);
+  return (await inquirer.default.prompt(preguntas)).options;
 };
 
 const pausa = async () => {
-  const question = [
-    {
-      type: 'input',
-      name: 'enter',
-      message: `Presione la tecla ${'Enter'.green} para continuar`,
-    },
-  ];
-  console.log('\n');
-  await inquirer.prompt(question);
+  await inquirer.default.prompt([{ type: "input", name: "enter", message: `Presione ${"enter".green}` }]);
 };
 
-const leerInput = async (message) => {
-  const question = [
-    {
-      type: 'input',
-      name: 'desc',
-      message,
-      validate(value) {
-        if (value.length === 0) {
-          return 'Por favor ingrese un valor';
-        }
-        return true;
-      },
-    },
-  ];
-
-  const { desc } = await inquirer.prompt(question);
-  return desc;
+const ejecutarMenu = async () => {
+  let opcion;
+  
+  const manager = new TareaManager();
+  do {
+    opcion = await menu();
+    if (opcion === "1") {
+      const { nuevaTarea } = await inquirer.default.prompt([{ type: "input", name: "nuevaTarea", message: "Descripcion: " }]); 
+      
+      await manager.crearTarea(nuevaTarea);
+      console.log("Tarea creada correctamente.".green);
+    } else if (opcion === "2") {
+      const tareasListadas = await manager.listarTareas();
+      console.log("Listado de tareas:".blue);
+      console.log(tareasListadas);
+    } else if (opcion === "3") {
+const tareasCompletas = await manager.listarTareasCompletas();
+        console.log("Listado de tareas completas:".blue);
+        console.log(tareasCompletas);
+    } else if (opcion === "4") {
+        const tareasPendientes = await manager.listarTareasPendientes();
+        console.log("Listado de tareas pendientes:".blue);
+        console.log(tareasPendientes);
+    } else if (opcion === "5") {
+        const tareacomp = await manager.listarTareasPendientes();
+        const { idTarea } = await inquirer.default.prompt([{
+          type: "list",
+          name: "idTarea",
+          message: "Seleccione la tarea a completar:",
+          choices: tareacomp.map((tarea, index) => ({ 
+            value: tarea.id,  // Obtén el id de la tarea
+            name: `${index + 1}. ${tarea.descripcion}` 
+          })),
+        }]);
+        await manager.cambiarEstadoTarea(idTarea);
+        console.log("Tarea completada con exito.".green);
+    } else if (opcion === "6") {
+      const tareasBorrar = await manager.listarTareas();
+const { idTarea } = await inquirer.default.prompt([{
+  type: "list",
+  name: "idTarea",
+  message: "Seleccione la tarea a borrar:",
+  choices: tareasBorrar.map((tarea, index) => ({ 
+    value: tarea.id,  // Obtén el id de la tarea
+    name: `${index + 1}. ${tarea.descripcion}` 
+  })),
+}]);
+await manager.borrarTarea(idTarea);
+console.log("Tarea borrada correctamente.".green);
+    } else if (opcion === "0") {
+      console.log("Saliendo...".blue);
+      process.exit();
+    } else {
+      console.log("Opción inválida.".red);
+    }
+    await pausa(); 
+  } while (opcion !== "0");
 };
 
-module.exports = {
-  menu,
-  pausa,
-  leerInput,
-};
+module.exports = { menu, ejecutarMenu, pausa };
